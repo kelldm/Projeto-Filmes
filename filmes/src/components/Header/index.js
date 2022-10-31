e098b7c9ac600b00debfa81f451d227eba47cd1f
Raquel Lins <raquellinsdev@gmail.com>
2022-10-31 16:53:17 -0300

    deu b.o rapaize
    correção dos b.o

--- filmes/src/components/Header/index.js
+++ filmes/src/components/Header/index.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import './header.css';
+import {Link}  from 'react-router-dom'
+
 function Header(){
     return(
         <header>
-            <h1>Teste HEADER</h1>
+            <Link className='logo' to='/'>Filmaplix!</Link>
+            <Link className='favorites' to='favorites'>Meus filmes</Link>
         </header>
 
     )
 }
+
+export default Header;
---
+++ filmes/src/services/api.js
@@ -0,0 +1,2 @@
+
+// URL DA API http://api.themoviedb.org/3/movie/550?api_key=52c43ee20b41d87e742e5ccb664b1ef9
